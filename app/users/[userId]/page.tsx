2cd84ca02539a040b43ce8b472c27e04451dad99
tavolafourcade <tavolafourcade@gmail.com>
2023-07-09 18:39:55 -0500

    [ ADD ] Dynamic generation of metadata for each user

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -2,12 +2,24 @@ import getUser from '@/app/lib/getUser'
 import getUserPosts from '@/app/lib/getUserPosts'
 import { Suspense } from 'react'
 import UserPosts from './components/UserPosts'
+import type { Metadata } from 'next'
 
 type Params = {
   params: {
     userId: string
   }
 }
+
+const generateMetadata = async({ params: {userId}}: Params): Promise<Metadata> => {
+  const userData: Promise<User> = getUser(userId)
+  const user: User = await userData
+
+  return {
+    title: user.name,
+    description: `This is the page of ${user.name}`,
+  }
+}
+
 const UserPage = async ({ params: {userId}}: Params) => {
   // Notice that we are requesting this data in paralell
   const userData: Promise<User> = getUser(userId)
@@ -28,4 +40,5 @@ const UserPage = async ({ params: {userId}}: Params) => {
     </>
   )
 }
+export { generateMetadata, UserPage }
 export default UserPage
